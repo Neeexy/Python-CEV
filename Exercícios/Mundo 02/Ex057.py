e2c4fafb390003282065375a9e23131af7bd75ba
Neeexy <nexy.2809@gmail.com>
2024-08-04 00:01:15 -0300

    a

--- Exercícios/Mundo 02/Ex057.py
+++ Exercícios/Mundo 02/Ex057.py
@@ -1,10 +1,5 @@
 '''Exercício Python 057: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.'''
-sexo = []
-while sexo != 'M' or 'F':
-    sexo = str(input('Informe seu sexo [M/F]: ')).upper()
-    if sexo != 'M' or 'F':
-        print('Por favor insira seu sexo corretamente [M/F]')
-if sexo == 'M':
-    print(f'Seu sexo é {sexo}')
-else:
-    print(f'Seu sexo é {sexo}')
+sexo = str(input('Informe seu sexo [M/F]: ')).upper()[0].strip()
+while sexo not in 'MfmF':
+    sexo = str(input('Dados inválidos. Por favor, informe seus dados corretamente: ')).upper()[0].strip()
+print(f'Sexo {sexo} registrado com sucesso!')
