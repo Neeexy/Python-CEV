4f9cf1cd4b6fe58c3763e8fa25ee0c212d564805
Neeexy <nexy.2809@gmail.com>
2024-08-09 04:00:09 -0300

    .

--- Exercícios/Mundo 02/Ex058.py
+++ Exercícios/Mundo 02/Ex058.py
@@ -1,25 +1,25 @@
 '''Exercício Python 058: Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.'''
-
 # Passo 1 - Importar Bibliotecas ( random )
 # Passo 2 - Gerar Números do computador
 # Passo 3 - Inicializar um contador de tentativar
 # Passo 4 - Loop
-
 # ------
-print('Vamos jogar um jogo de advinhação!\n')
 from random import randint as rd
+print('-------- Vamos jogar um jogo de advinhação! Vou pensar num número aleatório entre 0 e 50. --------')
 
-num_pc = rd(0, 10)
+num_pc = rd(0, 50)
 tentativas = 0
-print(num_pc)
+max = 6
 while True:
-    call = int(input('Tente advinhar um número aleatório: '))
+    call = int(input('\nTente advinhar um número aleatório: '))
     tentativas += 1
     if call == num_pc:
-        print(f'Parabéns, você acertou com {tentativas} tentativas.')
+        print(f'\nParabéns, você acertou com {tentativas} tentativas.')
         break
+    elif call > num_pc:
+        print(f'Um pouco menos, tente novamente! Você usou {tentativas} tentativas de {max}.')
     else:
-        if call > num_pc:
-            print('Um pouco menos, tente novamente!')
-        else:
-            print('Um pouco mais, tente novamente!')
+        print(f'Um pouco mais, tente novamente! Você usou {tentativas} tentativas de {max}.')
+    if tentativas == max:
+        print(f'\nInfelizmente você não acertou o número que eu pensei, ele era o {num_pc}.')
+        break
