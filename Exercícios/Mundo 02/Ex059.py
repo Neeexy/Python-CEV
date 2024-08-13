040b29d481fc8d1fdd0555705c46454fe23c52cd
Neeexy <nexy.2809@gmail.com>
2024-08-12 22:42:01 -0300

    a

--- Exercícios/Mundo 02/Ex059.py
+++ Exercícios/Mundo 02/Ex059.py
@@ -6,51 +6,57 @@
 [ 5 ] sair do programa
 Seu programa deverá realizar a operação solicitada em cada caso.'''
 import os
-from time import sleep
-
-def print_animado(msg, time=0.10):
-    linhas = msg.split('\n')
-    for linha in linhas:
-        palavras = linha.split()  # Divide a linha atual em palavras
-        for palavra in palavras:
-            print(palavra, end=' ', flush=True)
-            sleep(time)
-        print()  # Move para a próxima linha
-
-valor_1 = float(input('Digite um primeiro valor: '))
-valor_2 = float(input('Digite um segundo valor: '))
-while True:
+def limpar():
     os.system('cls')
+def valor(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print('=-= '*20)
+            print('Opção inválida. Por favor, insira um número inteiro.')
+            print('=-= '*20)
 
+valor_1 = valor('Digite um primeiro valor: ')
+valor_2 = valor('Digite um segundo valor: ')
+op = 0
+while op != 5:
+    limpar()
     print(f'\nOs valores são: |{valor_1}| e |{valor_2}|')
-    menu = '''
-    Selecione uma das opções abaixo:
-
+    print('''\nSelecione uma das opções abaixo
         [ 1 ] Somar
         [ 2 ] Multiplicar
         [ 3 ] Maior
         [ 4 ] Inserir novos números
-        [ 5 ] Sair do Programa
-
-    '''
-    print_animado(menu)
-    op = str(input('Selecione sua opção: '))
-    if op == '1':
+        [ 5 ] Sair do Programa''')
+    try:
+        op = int(input('Selecione sua opção: '))
+    except ValueError:
+        print('=-= '*20)
+        print('Opção inválida. Por favor, insira um número inteiro.')
+        print('=-= '*20)
+        input("\nPressione ENTER para continuar...")
+        continue
+    if op == 1:
         soma = valor_1 + valor_2
         print(f'\nA soma dos valores inseridos é: {soma}. ')
-    elif op == '2':
+    elif op == 2:
         mul = valor_1 * valor_2
         print(f'\nO resultado da multiplicação dos valores inseridos é: {mul}. ')
-    elif op == '3':
-        maior = max(valor_1, valor_2)
+    elif op == 3:
+        maior = max(valor_1,valor_2)
         print(f'O maior valor entre os inseridos é: [{maior}]')
-    elif op == '4':
-        valor_1 = float(input('Digite o novo primeiro valor: '))
-        valor_2 = float(input('Digite o novo segundo valor: '))
-    elif op == '5':
-        os.system('cls')
+    elif op == 4:
+        valor_2 = valor('Digite um novo primeiro valor: ')
+        valor_2 = valor('Digite um novo segundo valor: ')
+    elif op == 5:
+        limpar()
+        print('=-= '*20)
         print('\nObrigado, até mais!')
+        print('=-= '*20)
         break
     else:
-        print('\nOpção inválida, tente novamente.')
+        print('=-= '*20)
+        print('Opção inválida, tente novamente.')
+        print('=-= '*20)
     input("\nPressione ENTER para continuar...")
