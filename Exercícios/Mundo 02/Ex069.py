723e34b5d6c11df9fd68e5eb7fd53724d1e4a5d3
Neeexy <nexy.2809@gmail.com>
2024-08-22 15:29:38 -0300

    a

---
+++ Exercícios/Mundo 02/Ex069.py
@@ -0,0 +1,44 @@
+'''Exercício Python 069: Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:
+A) quantas pessoas tem mais de 18 anos.
+B) quantos homens foram cadastrados.
+C) quantas mulheres tem menos de 20 anos. '''
+
+idade18 = f = m = fn = pessoa =0
+msg = 'CADASTRE UMA PESSOA'
+while True:
+    print('-'*35)
+    print(f'{msg:^33}',f' [{pessoa}]')
+    print('-'*35)
+
+    idade = int(input('Idade: '))
+    if idade > 18:
+        idade18 += 1
+
+    sexo = str(input('Sexo: [M/F] ')).upper().strip() [0]
+
+    if sexo == 'M':
+        m += 1
+
+    elif sexo == 'F':
+        f += 1
+        if idade < 20:
+            fn += 1
+
+    else:
+        print('Sexo inválido.')
+        break
+    
+    pessoa += 1
+
+    confirmação = str(input('Quer continuar? [S/N]\n')).upper().strip() [0]
+    if confirmação == 'N':
+        break
+    else:
+        continue
+
+print('-'*35)
+print(f'Total de pessoas cadastradas : {pessoa}')
+print(f'Total de homens cadastrados: {m}')
+print(f'Total de pessoas com mais de 18 anos: {idade18}')
+print(f'Total de mulheres com menos de 20 anos cadastradas: {fn}')
+print('-'*35)
