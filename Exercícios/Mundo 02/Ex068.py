456647ac55c90033680d2e7684b3a15e4afbd006
Neeexy <nexy.2809@gmail.com>
2024-08-22 14:02:13 -0300

    a

--- Exercícios/Mundo 02/Ex068.py
+++ Exercícios/Mundo 02/Ex068.py
@@ -3,9 +3,31 @@ from random import randint as rd
 print('=-='*20)
 print('VAMOS JIGAR PAR OU ÍMPAR')
 print('=-='*20)
-
+jogada = 0
+wins = 0
 while True:
     jogada = int(input('Diga um valor: '))
-    par_impar = str(input('Par ou Ímpar? [P/I] ')).upper().strip() [0]
-    if par_impar == 'P':
-        jogada
+    comp = rd(0, 10)
+    print(comp)
+    tipo = ' '
+    while tipo not in 'IiPp':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).upper().strip() [0]
+    rodada =  comp + jogada
+    print(f'Você jogou {jogada} e o computador jogou {comp}. Total de {rodada} ', end='')
+    print('DEU PAR' if rodada %2 ==0 else 'DEU IMPAR')
+    if tipo == 'P':
+        if rodada %2 == 0:
+            print('Você VENCEU!!')
+            wins+=1
+        else:
+            print('Você PERDEU!!')
+            break
+    if tipo == 'I':
+        if rodada %2 == 1:
+            print('Você VENCEU!!')
+            wins+=1
+        else:
+            print('Você PERDEU!!')
+            break
+                
+print(f'GAME OVER! Você venceu {wins} vezes.')
