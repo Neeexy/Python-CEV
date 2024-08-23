bcf1028a64c1e31aa873febd1ca4954490498d43
Neeexy <nexy.2809@gmail.com>
2024-08-23 17:46:32 -0300

    Aula 16

--- Aulas/aula16.py
+++ Aulas/aula16.py
@@ -1,2 +1,20 @@
-lanche = ('Hamburger', 'Suco', 'Pizza', 'Pudim')
-print (lanche[0:])
+# lanche = ('Hamburger', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+# Tuplas são imutáveis
+'''for comida in lanche:
+
+    print(comida,end=' ')
+
+print('\n')
+
+for cont in range (0, len(lanche)):
+    print(f'{lanche[cont]} na posição {cont+1}')
+
+print()
+
+for posição, comida in enumerate(lanche):
+    print(f'Vou comer por {posição+1}° o {comida}')'''
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a+b
+print(c)
