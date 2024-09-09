21dcda65791c003281442852edc3576e929f59df
Neeexy <nexy.2809@gmail.com>
2024-09-09 16:32:26 -0300

    Update Ex84.py

--- Exercícios/Mundo 03/Ex84.py
+++ Exercícios/Mundo 03/Ex84.py
@@ -3,16 +3,33 @@ A) Quantas pessoas foram cadastradas.
 B) Uma listagem com as pessoas mais pesadas.
 C) Uma listagem com as pessoas mais leves.'''
 
-lista_temporaria = []
-lista_principal = []
-
+dados = []
+pessoas = []
+ma = me = 0
 while True:
-    lista_temporaria.append(str(input('Nome: ')))
-    lista_temporaria.append(float(input('Peso: ')))
-    lista_principal.append(lista_temporaria[:])
-    lista_temporaria.clear()
-    continuar = input('Quer continuar? [S/N]').upper().split()[0]
+    dados.append(str(input('Nome: ')))
+    dados.append(float(input('Peso: ')))
+    if len(pessoas) == 0:
+        ma = me = dados[1]
+    else:
+        if dados[1] > ma:
+            ma = dados[1]
+        if dados[1] < me:
+            me = dados[1]
+    pessoas.append(dados[:])
+    dados.clear()
+    continuar = str(input('Quer continuar? [S/N]')).upper().split()[0]
     if continuar == 'N':
         break
-
-print(f'Os dados foram {lista_principal}')
+print('=-=' *20)
+print(f'Ao todo, foram cadastradas {len(pessoas)} pessoas.')
+print('O mais pesados foram :',end='')
+for p in pessoas:
+    if p[1] == ma:
+        print(f'{p[0]},',end=' ')
+print(f'Pesando: {ma}Kg')
+print('Os mais leves foram: ',end='')
+for p in pessoas:
+    if p[1] == me:
+        print(f'{p[0]},',end=' ')
+print(f'Pesando: {me}Kg')
